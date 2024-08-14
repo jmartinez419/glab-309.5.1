9439408c233cf0885b81207e91ac2f4cbdab7cf8
jmartinez419 <justynmartinez419@gmail.com>
2024-08-14 12:28:24 -0400

    did glab309.5.2, used postman to test endpoints

--- src/main/java/com/perscholas/ExecerciseOne/Controller/TestController/controllerstwo/MyControllertwo.java
+++ src/main/java/com/perscholas/ExecerciseOne/Controller/TestController/controllerstwo/MyControllertwo.java
@@ -1,4 +1,4 @@
-package com.perscholas.ExecerciseOne.Rest.controllerstwo;
+package com.perscholas.ExecerciseOne.Controller.TestController.controllerstwo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/com/perscholas/ExecerciseOne/Rest/Application.java
+++ src/main/java/com/perscholas/ExecerciseOne/Rest/Application.java
@@ -3,7 +3,7 @@ package com.perscholas.ExecerciseOne.Rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.perscholas.ExecerciseOne")
 public class Application {
 
 	public static void main(String[] args) {
