84ed4e07d04bf5dea3db6812944c4bc7392ff678
jmartinez419 <justynmartinez419@gmail.com>
2024-08-14 16:00:32 -0400

    did glab 309.5.7

--- src/main/java/com/perscholas/ExecerciseOne/model/User.java
+++ src/main/java/com/perscholas/ExecerciseOne/model/User.java
@@ -1,10 +1,63 @@
 package com.perscholas.ExecerciseOne.model;
+//
+//import lombok.AccessLevel;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//import lombok.experimental.FieldDefaults;
+//
+//@NoArgsConstructor
+//@FieldDefaults(level = AccessLevel.PRIVATE)
+//public class User {
+//    int id;
+//    String name;
+//    String email;
+//
+//    public User(int id, String name, String email) {
+//        this.id = id;
+//        this.name = name;
+//        this.email = email;
+//    }
+//
+//    public User(int id, String name) {
+//        this.id = id;
+//        this.name = name;
+//    }
+//    public int getId() {
+//        return id;
+//    }
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//    public String getEmail() {
+//        return email;
+//    }
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//    @Override
+//    public String toString() {
+//        return "User{" +
+//                "id=" + id +
+//                ", name='" + name + '\'' +
+//                ", email='" + email + '\'' +
+//                '}';
+//    }
+//}
 
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
+//@Data
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class User {
@@ -22,9 +75,11 @@ public class User {
         this.id = id;
         this.name = name;
     }
+
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
@@ -36,12 +91,15 @@ public class User {
     public void setName(String name) {
         this.name = name;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     @Override
     public String toString() {
         return "User{" +
